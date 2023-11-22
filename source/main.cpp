3c489549460adf425c3d78130dd267e5160c80ca
Iscander <iscander161@yandex.ru>
2023-11-22 16:09:32 +0300

    Static build. No waiting exit.

--- source/main.cpp
+++ source/main.cpp
@@ -12,20 +12,35 @@
 #include "app_args.h"
 #include "rxi_utils.h"
 
+/* Private function prototypes -----------------------------------------------*/
+static int app_exit();
+
 /**
  * @brief Main function of application
  * @param argc - number of arguments
  * @param argv - array of arguments
  * @return - result of function
  */
-int main(int argc, char** argv)
+int main(int argc, char** argvs)
 {
-    if(argc == 0x02){
+    if(argc == 0x01){
         std::cout << "No arguments provided. Nothing to do yet." << std::endl;
-        std::cout << "Press any key to continue..." << std::endl;
-        getchar();
         return EXIT_SUCCESS;
     }
-    rxiUtilNiosQSysXml("cpu.qsys", "D:\\Folder", "dps_ctrl_ch16");
+
+    if(argc != 0x04){
+        std::cout << "Arguments error." << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    rxiUtilNiosQSysXml(argvs[1], argvs[2], argvs[3]);
+
+    return EXIT_SUCCESS;
+}
+
+static int app_exit()
+{
+    std::cout << "Press any key to continue..." << std::endl;
+    getchar();
     return EXIT_SUCCESS;
 }
